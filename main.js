f5af2be2d29a0187754a6fc54f0fdb2a1b595edc
Anderson Marlon <anderson18.marlon@gmail.com>
2024-06-11 15:34:01 -0300

    Added: Pages - 11-06 15:34

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 import './style.css'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-  </div>
 `
-
-setupCounter(document.querySelector('#counter'))
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                captura: 'captura.html',
+                obrigado: 'obrigado.html',
+                vendasabertas: 'vendas-abertas.html',
+            },
+        },
+    },
+});
